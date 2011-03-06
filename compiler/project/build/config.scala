c549a6474cbbc4742b4d5a12b7a23cff0505cd4c
Erick Fleming <erick@sudogs.com>
2011-03-06 17:51:45 -0600

    applying new name

--- compiler/project/build/config.scala
+++ compiler/project/build/config.scala
@@ -2,17 +2,17 @@ import sbt._
 
 class ProjectConfig(info:ProjectInfo) extends DefaultProject(info) {
 
-    val s2jsClasspath = managedClasspath(config("test")) +++ buildLibraryJar
+    val scalosureClasspath = managedClasspath(config("test")) +++ buildLibraryJar
     
     override def repositories = Set(ScalaToolsSnapshots)
 
     override def libraryDependencies = super.libraryDependencies ++ Set(
-        "s2js"          %% "s2js-api"  % "0.2-SNAPSHOT" % "test",
-        "org.scalatest"  % "scalatest" % "1.3"          % "test")
+        "org.scalosure"  %% "scalosure-adapters" % "0.2-SNAPSHOT" % "test",
+        "org.scalatest"   % "scalatest"          % "1.3"          % "test")
 
     override def testOptions = super.testOptions ++ Seq(
-        TestArgument(TestFrameworks.ScalaTest, "-Dcp="+s2jsClasspath.getFiles.mkString(":")),
-        TestArgument(TestFrameworks.ScalaTest, "-Doutput="+(outputPath / "s2js")))
+        TestArgument(TestFrameworks.ScalaTest, "-Dcp="+scalosureClasspath.getFiles.mkString(":")),
+        TestArgument(TestFrameworks.ScalaTest, "-Doutput="+(outputPath / "scalosure")))
 }
 
 // vim: set ts=4 sw=4 et:
