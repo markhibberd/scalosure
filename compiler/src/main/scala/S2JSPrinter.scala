4628462971d77778575b4ed15716117576370663
Erick Fleming <erick@sudogs.com>
2011-03-13 14:58:48 -0500

    handle map and hashmaps are the same

--- compiler/src/main/scala/S2JSPrinter.scala
+++ compiler/src/main/scala/S2JSPrinter.scala
@@ -195,10 +195,7 @@ trait S2JSPrinter {
         case x @ Apply(TypeApply(y @ Select(Select(_, n), _), _), args) if(n.toString == "Array") =>
           args.map(buildObjectLiteral).mkString("[",",","]")
 
-        case x @ Apply(TypeApply(y @ Select(Select(_, n), _), _), args) if(n.toString == "HashMap") =>
-          "new %s(%s)".format(n, args.map(buildObjectLiteral).mkString("{",",","}"))
-
-        case x @ Apply(TypeApply(y @ Select(Select(_, n), _), _), args) if(n.toString.matches("Map")) =>
+        case x @ Apply(TypeApply(y @ Select(Select(_, n), _), _), args) if(n.toString.matches("(Map|HashMap)")) =>
           "new HashMap(%s)".format(args.map(buildObjectLiteral).mkString("{",",","}"))
 
         case x @ Apply(TypeApply(y @ Select(Select(_, n), _), _), args) if(n.toString.matches("Tuple[0-9]+")) => 
