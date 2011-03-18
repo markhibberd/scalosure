012dd655cd0b2454f7b2c273d92bea20cacba4cf
Erick Fleming <erick@sudogs.com>
2011-03-18 07:04:54 -0500

    cleanup

--- adapters/src/main/scala/package.scala
+++ adapters/src/main/scala/package.scala
@@ -2,14 +2,13 @@ package s2js
 
 case class Html(elem:xml.Elem) extends browser.Element
 
+class JsObject
+
 object JsObject {
   def apply(elems:(String,Any)*) = null
 }
 
 class JsArray {
-  //def indexOf()
-  //def lastIndexOf()
-  //def every()
   def every(fn:(Any, Long, JsArray)=>Boolean) = false
   def forEach(fn:(Any, Long, JsArray)=>Unit) {}
   def map(fn:(Any, Long, JsArray)=>Any) = new JsArray
