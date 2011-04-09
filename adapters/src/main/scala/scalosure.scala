c42026220f44f62020e84030034f1920ba0863c1
Erick Fleming <erick@sudogs.com>
2011-04-08 19:39:59 -0500

    jsobject should be in scalosure namespace

--- adapters/src/main/scala/scalosure.scala
+++ adapters/src/main/scala/scalosure.scala
@@ -4,4 +4,16 @@ object script {
   def literal(script:String):Unit = {}
 }
 
+class JsObject[A] {
+  var x:A = _
+  def foreach(fn:((String,A))=>Unit):Unit = {}
+  def apply(name:String):A = x
+  def update(key:String, value:A):Unit = {}
+}
+
+object JsObject {
+  def apply[A](elems:(String,A)*):JsObject[A] = new JsObject[A]
+  def empty[A]():JsObject[A] = new JsObject[A]
+}
+
 class ScalosureObject
