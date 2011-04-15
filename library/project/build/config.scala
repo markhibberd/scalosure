41c80f65ae58181acd90195c509271259a97733a
Erick Fleming <erick@sudogs.com>
2011-04-15 16:16:21 -0500

    starting to remove closure depencency

--- library/project/build/config.scala
+++ library/project/build/config.scala
@@ -10,11 +10,11 @@ class ProjectConfig(info: ProjectInfo) extends DefaultWebProject(info) with Scal
 
   override def libraryDependencies = super.libraryDependencies ++ Set(
 
-    "org.scalosure"  %% "scalosure-tools"    % "0.2-SNAPSHOT",
-    "org.scalosure"  %% "scalosure-adapters" % "0.2-SNAPSHOT",
-    "org.scalatest"  %  "scalatest"          % "1.3" % "test",
+    "org.scalosure" %% "scalosure-tools"    % "0.2-SNAPSHOT",
+    "org.scalosure" %% "scalosure-adapters" % "0.2-SNAPSHOT",
+    "org.scalatest" %  "scalatest"          % "1.3" % "test",
 
     /* java dependencies */
-    "org.eclipse.jetty"    % "jetty-webapp"          % "7.0.2.RC0" % "test",
-    "javax.servlet"        % "servlet-api"           % "2.5"       % "provided")
+    "org.eclipse.jetty" % "jetty-webapp" % "7.0.2.RC0" % "test",
+    "javax.servlet"     % "servlet-api"  % "2.5"       % "provided")
 }
