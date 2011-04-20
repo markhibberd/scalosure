4eb3c49b9c52afcb5873a6ff490b5d8ea9a87b04
Erick Fleming <erick@sudogs.com>
2011-04-20 09:32:52 -0500

    tdd spec should be empty

--- compiler/src/test/scala/TestDrivenSpecs.scala
+++ compiler/src/test/scala/TestDrivenSpecs.scala
@@ -5,35 +5,15 @@ import org.scalatest.{ Spec, BeforeAndAfterAll }
 
 class TestDrivenSpecs extends PrinterFixtureSpec {
 
-  it("") {
+  ignore("1") {
 
     testdriving = true
 
     parser expect {"""
-    import scalosure.JsArray
 
-    class F(xs:JsArray[Any]) {
-      def fe() {
-        var i = 0
-        while(i < xs.length) {
-            println(xs(i))
-            i = i + 1
-        }
-      }
-    }
-
-    object o {
-      def start() = {
-        val zs = JsArray.empty[String]
-        zs.push("foo")
-        println(zs(0))
-      }
-    }
     """} toDebug {"""
-    goog.provide('o');
-    o.start = function() {
-      var self = this;
-    };
+
     """}
+
   }
 }
