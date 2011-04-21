205cb93bc3e93aa8dd50240416b7b7a03e48aed8
Erick Fleming <erick@sudogs.com>
2011-04-21 07:39:34 -0500

    clean old testing code

--- library/src/main/scala/testing/app.scala
+++ library/src/main/scala/testing/app.scala
@@ -2,49 +2,10 @@ package testing
 
 import browser._
 
-import goog.dom._
-import goog.events
-import goog.events.Event
-
-import s2js._
-
-import scalosure.datastore.{ Entity, Collection => Col }
-
 object app {
-
-  def printit(e:Entity) = e match {
-      case Tenant("bar") => println("tenant: bar"); ""
-      case _ => println("what"); ""
-  }
 
   @s2js.ExportSymbol
   def start() = {
     
-    val t = testing.Tenant("foo")
-
-    val c = Col("one"->Tenant("foo"), "two"->Tenant("bar"))
-
-    c foreach { x => printit(x._2) }
-
   }
-
-  //def foo() {
-
-    //val x = goog.json.parse("{\"name\":\"foo\"}")
-
-    //val clickit = goog.dom.getElement("clickit")
-    //val message = goog.dom.getElement("message")
-    //val xio = new scalosure.net.XhrIo("http://localhost:8080/sample.json", "GET")
-
-    //xio.onComplete = { e => 
-      //val m = new HashMap(e.responseAs[JsObject[Sample]])
-      //val xs = m filter { x => x._2.name.startsWith("go") } map { x => x._2 }
-      //println(xs)
-    //}
-
-    //events.listen(clickit, "click", (e:Event) => { 
-      //xio.send(null)
-    //});
-  //}
-
 }
